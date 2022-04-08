8fc2a5e338f054f37cf2cd2049f3eb8ff13852cf
Sergey Zinyaev <zinyaevsi@gmail.com>
2022-04-08 16:21:08 +0500

    08.04.22

--- recreate_db.py
+++ recreate_db.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 now = datetime.now()
 # tables = [User, Order, Post, Shop, Error, Email_message, Users_addresses, Package, Tag_of_shops, Tag]
-tables = []
+tables = [Shop, Tag_of_shops]
 
 db.drop_tables(tables)
 db.create_tables(tables)
